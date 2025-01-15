49878fbd86c0dda6629d851a460982dcffbe109b
SchoergendorferLuis07 <l.schoergendorfer@students.htl-leonding.ac.at>
2025-01-15 14:37:12 +0100

    feat: added new design to frontend sensor

--- Frontend/Sensor/server.js
+++ Frontend/Sensor/server.js
@@ -4,12 +4,10 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-// Express-Server erstellen
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// Statische Dateien aus dem 'public'-Ordner servieren
 app.use(express.static(path.join(__dirname, 'public')));
 
 const mqttClient = mqtt.connect({
@@ -21,10 +19,8 @@ const mqttClient = mqtt.connect({
     rejectUnauthorized: true
 });
 
-// Objekt zum Speichern der ersten empfangenen Werte für jedes Topic
 const initialValues = {};
 
-// MQTT-Verbindung und Abonnement
 mqttClient.on('connect', () => {
     console.log('Verbunden mit MQTT-Broker');
     mqttClient.subscribe('#', (err) => {
@@ -36,17 +32,14 @@ mqttClient.on('connect', () => {
     });
 });
 
-// Empfange MQTT-Nachrichten und speichere den ersten Wert für jedes Topic
 mqttClient.on('message', (topic, message) => {
     const parsedMessage = JSON.parse(message.toString());
     console.log(`Topic: ${topic}, Message: ${message.toString()}`);
 
-    // Speichere nur den ersten Wert für jedes Topic, falls er noch nicht vorhanden ist
     if (!(topic in initialValues)) {
         initialValues[topic] = parsedMessage;
     }
 
-    // Sende die Nachricht an alle verbundenen WebSocket-Clients
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify({ topic, message: parsedMessage }));
@@ -54,11 +47,9 @@ mqttClient.on('message', (topic, message) => {
     });
 });
 
-// WebSocket-Verbindung
 wss.on('connection', (ws) => {
     console.log('Neue WebSocket-Verbindung hergestellt');
 
-    // Sende alle initialen Werte an den neuen Client
     for (const [topic, message] of Object.entries(initialValues)) {
         ws.send(JSON.stringify({ topic, message }));
     }
@@ -66,7 +57,6 @@ wss.on('connection', (ws) => {
     ws.send(JSON.stringify({ message: "Verbunden mit dem Server" }));
 });
 
-// Starte den Server
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server läuft auf http://localhost:${PORT}`);
