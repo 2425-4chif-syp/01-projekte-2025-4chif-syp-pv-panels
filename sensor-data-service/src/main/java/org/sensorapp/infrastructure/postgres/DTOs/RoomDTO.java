02017607005f74a59a95199aa2dd3716c29f3dd3
KletschTobias <t.kletsch@gmx.at>
2025-03-10 11:20:19 +0100

    fix: pvpanels-69 warnings ausgebessert

--- sensor-data-service/src/main/java/org/sensorapp/infrastructure/postgres/DTOs/RoomDTO.java
+++ sensor-data-service/src/main/java/org/sensorapp/infrastructure/postgres/DTOs/RoomDTO.java
@@ -16,7 +16,6 @@ public class RoomDTO {
     private Integer neighbourOutsideId;
     private String direction;
 
-    // **Füge diesen Konstruktor hinzu**
     public RoomDTO(int roomId, String roomLabel, String roomName, String roomType,
                    Integer corridorId, Integer neighbourInsideId, Integer neighbourOutsideId, String direction) {
         this.roomId = roomId;
@@ -26,72 +25,6 @@ public class RoomDTO {
         this.corridorId = corridorId;
         this.neighbourInsideId = neighbourInsideId;
         this.neighbourOutsideId = neighbourOutsideId;
-        this.direction = direction;
-    }
-
-
-    // Getter & Setter
-    public int getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-
-    public String getRoomLabel() {
-        return roomLabel;
-    }
-
-    public void setRoomLabel(String roomLabel) {
-        this.roomLabel = roomLabel;
-    }
-
-    public String getRoomName() {
-        return roomName;
-    }
-
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
-    }
-
-    public String getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
-
-    public Integer getCorridorId() {
-        return corridorId;
-    }
-
-    public void setCorridorId(Integer corridorId) {
-        this.corridorId = corridorId;
-    }
-
-    public Integer getNeighbourInsideId() {
-        return neighbourInsideId;
-    }
-
-    public void setNeighbourInsideId(Integer neighbourInsideId) {
-        this.neighbourInsideId = neighbourInsideId;
-    }
-
-    public Integer getNeighbourOutsideId() {
-        return neighbourOutsideId;
-    }
-
-    public void setNeighbourOutsideId(Integer neighbourOutsideId) {
-        this.neighbourOutsideId = neighbourOutsideId;
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String direction) {
         this.direction = direction;
     }
 }
